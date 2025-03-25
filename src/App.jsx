17ddb8daf2b9ccc4f11850a6d5b7d5efea184c9e
Ayman Shalaby <aymanshalaby539@gmail.com>
2025-03-25 02:11:15 +0200

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
-import CityList from "./components/CityList";
-import CountryList from "./components/CountryList";
-import City from "./components/City";
-import Form from "./components/Form";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import { lazy, Suspense } from "react";
+
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+const CityList = lazy(() => import("./components/CityList"));
+const CountryList = lazy(() => import("./components/CountryList"));
+const City = lazy(() => import("./components/City"));
+const Form = lazy(() => import("./components/Form"));
+const ProtectedRoute = lazy(() => import("./pages/ProtectedRoute"));
 
 import { CitiesProvider } from "./context/CitiesContext";
 import { AuthProvider } from "./context/AuthContext";
+import Spinner from "./components/Spinner";
 
 function App() {
   return (
@@ -20,18 +23,60 @@ function App() {
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="app" element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
+            <Route path="/" element={
+              <Suspense fallback={<Spinner />}>
+                <Homepage />
+              </Suspense>
+            } />
+            <Route path="app" element={
+              <Suspense fallback={<Spinner />}>
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              </Suspense>
+            }>
               <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
+              <Route path="cities" element={
+                <Suspense fallback={<Spinner />}>
+                  <CityList />
+                </Suspense>
+              } />
+              <Route path="cities/:id" element={
+                <Suspense fallback={<Spinner />}>
+                  <City />
+                </Suspense>
+              } />
+              <Route path="countries" element={
+                <Suspense fallback={<Spinner />}>
+                  <CountryList />
+                </Suspense>
+              } />
+              <Route path="form" element={
+                <Suspense fallback={<Spinner />}>
+                  <Form />
+                </Suspense>
+              } />
             </Route>
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="*" element={<PageNotFound />} />
+            <Route path="product" element={
+              <Suspense fallback={<Spinner />}>
+                <Product />
+              </Suspense>
+            } />
+            <Route path="login" element={
+              <Suspense fallback={<Spinner />}>
+                <Login />
+              </Suspense>
+            } />
+            <Route path="pricing" element={
+              <Suspense fallback={<Spinner />}>
+                <Pricing />
+              </Suspense>
+            } />
+            <Route path="*" element={
+              <Suspense fallback={<Spinner />}>
+                <PageNotFound />
+              </Suspense>
+            } />
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
